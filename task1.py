3c1f9642179af0af25a2a47561317b6ccc1b8327
2003analyst <tatuameir46@gmail.com>
2026-07-09 17:33:44 +0300

    Assignment 1

--- task1.py
+++ task1.py
@@ -1,10 +1,29 @@
+def introduce():
+    #input user information
+    first = str(input("Enter your first name: "))
+    second = str(input("Enter your second name: "))
+    third = str(input("Enter your third name: "))
+    age = int(input("Enter your age: "))
+    course = str(input("Enter your course: "))
+    year = int(input("Enter your year of study: "))
+    address = str(input("Enter your address: "))
+    hobby = str(input("Enter your hobby: "))
 
-first="Tatu"
-second="Ameir"
-third="Haji"
-age=25
-course="Computer Science"
-year = 2
-address="Jumbi"
-hobby="Coding"
-print("My name is " + first + " " + second + " " + third + ". I am " + str(age) + " years old. I am studying " + course + " in year " + str(year) + ". I live in " + address + ". My hobby is " + hobby + ".")
+    #print output
+    print("My name is " + first + " " + second + " " + third + ". I am " + str(age) + " years old. I am studying " + course + " in year " + str(year) + ". I live in " + address + ". My hobby is " + hobby)
+
+# Call the function to get user information
+introduce()
+
+# Grade checking
+grade = int(input("Enter your grade: "))
+if grade >= 90:
+    print("Your grade is A")
+elif grade >= 80:
+    print("Your grade is B")
+elif grade >= 70:
+    print("Your grade is C")
+elif grade >= 60:
+    print("Your grade is D")
+else:
+    print("Your grade is F")
